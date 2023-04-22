f22a90189ce09b8a886d85549cb7fe96ceaf4ebc
Zheng, Bang-Bo <zhengbangbo@hotmail.com>
2023-04-22 15:34:38 +0800

    feat: use token to increase API request limits

--- src/api/github.ts
+++ src/api/github.ts
@@ -1,12 +1,17 @@
 import { Octokit } from '@octokit/core'
+import * as dotenv from 'dotenv'
 import { getOwnerAndRepo } from '../utils/utils'
 
-const octokit = new Octokit()
+dotenv.config()
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
 
 export async function reqGithubStar(repoUrl: string) {
   const repo = getOwnerAndRepo(repoUrl)
+  // 如果请求报错，暂停一秒后再次请求
+
   const data = await octokit.request(`GET /repos/${repo}`)
-  if (!('stargazers_count' in data.data))
+
+  if (!data?.data?.stargazers_count)
     throw new Error('No GitHub stargazers count data')
   return data.data.stargazers_count
 }
